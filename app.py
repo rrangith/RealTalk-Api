751aed35ed35fc87006cf0a2bda15d4af3050c96
rrangith <rrangith@edu.uwaterloo.ca>
2019-06-02 22:08:54 -0400

    copying files froming files from rest api branch

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, jsonify, request, abort
+from flask_cors import CORS
+from video import Video
+import json
+
+app = Flask(__name__)
+CORS(app)
+app.config['DEBUG'] = True
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'hi'
+
+@app.route('/detect', methods=['GET'])
+def detect():
+    request.data.decode("utf-8").split(';base64,')[1][:-1]
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
