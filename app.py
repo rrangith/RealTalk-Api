ce9d8fad0ce76e15c133e91b8afc3596f131075a
rrangith <rrangith@edu.uwaterloo.ca>
2019-06-05 10:35:10 -0400

    adding endpoint for links and refactoring

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template, jsonify, request, abort
+from flask import Flask, jsonify, request, abort
 from flask_cors import CORS
-import json
 from detection import detectImage
 
 import base64
 import io
 from PIL import Image
-import cv2
+import requests
+from io import BytesIO
 
 app = Flask(__name__)
 CORS(app)
@@ -16,15 +16,15 @@ app.config['DEBUG'] = True
 def index():
     return 'hi'
 
-@app.route('/detectBinary', methods=['POST'])
-def detectBinary():
-    # try:
-    imageBinary = request.data.decode("utf-8").split(';base64,')[1][:-1]
-    img_data = base64.b64decode(str(imageBinary))
-    image = Image.open(io.BytesIO(img_data))
-    detectImage(image)
-    # except:
-    #     abort(400)
+# @app.route('/detectBinary', methods=['POST'])
+# def detectBinary():
+#     try:
+#         imageBinary = request.data.decode("utf-8").split(';base64,')[1][:-1]
+#         img_data = base64.b64decode(str(imageBinary))
+#         image = Image.open(BytesIO(img_data))
+#         detectImage(image)
+#     except:
+#         abort(400)
 
 @app.route('/detectFile', methods=['POST'])
 def detectFile():
@@ -32,5 +32,12 @@ def detectFile():
     response = detectImage(image)
     return jsonify(response)
 
+@app.route('/detectLink', methods=['POST'])
+def detectLink():
+    response = requests.get(request.get_json()['url'])
+    image = Image.open(BytesIO(response.content))
+    response = detectImage(image)
+    return jsonify(response)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- detection.py
+++ detection.py
@@ -1,13 +1,9 @@
-from utils import detector_utils as detector_utils
 import cv2
 import tensorflow as tf
-import datetime
-
-from statistics import mode
-
 from keras.models import load_model
 import numpy as np
 
+from utils import detector_utils as detector_utils
 from utils.datasets import get_labels
 from utils.inference import detect_faces
 from utils.inference import draw_text
@@ -16,74 +12,45 @@ from utils.inference import apply_offsets
 from utils.inference import load_detection_model
 from utils.preprocessor import preprocess_input
 
+"""
+Hand
+"""
+# Constants
+num_hands_detect = 2 # max number of hands we want to detect/track, can scale this up
+min_threshold = 0.2
+
+# Model
 detection_graph, sess = detector_utils.load_inference_graph()
 
-# parameters for loading data and images
+"""
+Face
+"""
+# Files
 detection_model_path = 'haarcascade_frontalface_default.xml'
 emotion_model_path = 'fer2013_mini_XCEPTION.102-0.66.hdf5'
 emotion_labels = get_labels('fer2013')
 
-
-num_hands_detect = 2 # max number of hands we want to detect/track, can scale this up
-min_threshold = 0.2
-
-global emotion_classifier
+# Loading models
+face_detection_model = load_detection_model(detection_model_path)
 emotion_classifier = load_model(emotion_model_path, compile=False)
-    # getting input model shapes for inference
-global graph
-graph = tf.get_default_graph()
+graph = tf.get_default_graph() # To avoid tensor flow graph not found error
 
+# Emotion constants
+emotion_target_size = emotion_classifier.input_shape[1:3]
+emotion_offsets = (20, 40)
 
+"""
+PUT STUFF HERE
+"""
 def detectImage(image):
-    emotion_target_size = emotion_classifier.input_shape[1:3]
+    im_width, im_height = image.size # Getting image height and width
+    image_np = np.array(image) # Converting PIL image to numpy array
 
-    # loading models
-    face_detection = load_detection_model(detection_model_path)
+    hand_coords = getHandCoords(image_np, im_width, im_height) # Get coordinates of hands in an array
 
-    emotion_offsets = (20, 40)
+    face_details = getFaceDetails(image_np) # Get emotion and coordinates of face
 
-    im_width, im_height = image.size
-
-    image_np = np.array(image)
-
-
-    image_np = cv2.flip(image_np, 1)
-    gray_image = cv2.cvtColor(image_np, cv2.COLOR_RGB2GRAY)
-
-
-
-    faces = detect_faces(face_detection, gray_image)
-
-    if len(faces) > 0:
-        draw_bounding_box(faces[0], image_np)
-        x1, x2, y1, y2 = apply_offsets(faces[0], emotion_offsets)
-        gray_face = gray_image[y1:y2, x1:x2]
-        gray_face = cv2.resize(gray_face, (emotion_target_size))
-        gray_face = preprocess_input(gray_face, True)
-        gray_face = np.expand_dims(gray_face, 0)
-        gray_face = np.expand_dims(gray_face, -1)
-        with graph.as_default():
-            emotion_prediction = emotion_classifier.predict(gray_face)
-        emotion_probability = np.max(emotion_prediction)
-        emotion_label_arg = np.argmax(emotion_prediction)
-    
-        face_details = [emotion_labels[emotion_label_arg], [int(faces[0][0]), int(faces[0][1]), int(faces[0][2]), int(faces[0][3])]]
-    else:
-        face_details = [None, [None, None, None, None]]
-
-
-    # Actual detection. Variable boxes contains the bounding box cordinates for hands detected,
-    # while scores contains the confidence for each of these boxes.
-    # Hint: If len(boxes) > 1 , you may assume you have found atleast one hand (within your score threshold)
-    boxes, scores = detector_utils.detect_objects(image_np, detection_graph, sess)
-
-    hand_coords = detector_utils.get_hand_coords(num_hands_detect, min_threshold, scores, boxes, im_width, im_height, image_np) #0.2 is the min threshold
-
-    if len(hand_coords) < 1:
-        hand_coords[0] = [None, None, None, None]
-    if len(hand_coords) < 2:
-        hand_coords[1] = [None, None, None, None]
-
+    # JSON structure to return
     image_details = {
                         "hands": [
                             {
@@ -108,10 +75,56 @@ def detectImage(image):
                         }
                     }
 
-    image_np = cv2.flip(image_np, 1)
-
-    cv2.imwrite('box.png', cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR))
+    cv2.imwrite('box.png', cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR)) # Writing to an image for testing purposes to prove boxes draw properly
     
     return image_details
 
+"""
+PUT STUFF HERE
+"""
+def getHandCoords(image_np, im_width, im_height):
+    # Actual detection. Variable boxes contains the bounding box cordinates for hands detected,
+    # while scores contains the confidence for each of these boxes.
+    boxes, scores = detector_utils.detect_objects(image_np, detection_graph, sess)
+
+    hand_coords = detector_utils.get_hand_coords(num_hands_detect, min_threshold, scores, boxes, im_width, im_height, image_np) # 0.2 is the min threshold
+
+    # This is in case hands are not found
+    if len(hand_coords) < 1:
+        hand_coords[0] = [None, None, None, None]
+    if len(hand_coords) < 2:
+        hand_coords[1] = [None, None, None, None]
+
+    return hand_coords
+ 
+"""
+PUT STUFF HERE
+"""
+def getFaceDetails(image_np):
+    gray_image = cv2.cvtColor(image_np, cv2.COLOR_RGB2GRAY) # the model requires a grayscale image
+
+    faces = detect_faces(face_detection_model, gray_image) # pass in the grayscale image
+
+    if len(faces) > 0: # make sure a face was found
+        draw_bounding_box(faces[0], image_np) # draws a square around the face, just for testing purposes
+        
+        # doing preprocessing on grayscale image, found from the model creator's example
+        x1, x2, y1, y2 = apply_offsets(faces[0], emotion_offsets) 
+        gray_face = gray_image[y1:y2, x1:x2]
+        gray_face = cv2.resize(gray_face, (emotion_target_size))
+        gray_face = preprocess_input(gray_face, True)
+        gray_face = np.expand_dims(gray_face, 0)
+        gray_face = np.expand_dims(gray_face, -1)
+        
+        with graph.as_default(): # to prevent graph not found error
+            emotion_prediction = emotion_classifier.predict(gray_face) # predict the emotion
+
+        #emotion_probability = np.max(emotion_prediction) # uncomment this line to see the accuracy of the emotion
+        
+        emotion_label_arg = np.argmax(emotion_prediction)
     
+        face_details = [emotion_labels[emotion_label_arg], [int(faces[0][0]), int(faces[0][1]), int(faces[0][2]), int(faces[0][3])]] # structure to return emotion and face coords
+    else:
+        face_details = [None, [None, None, None, None]] # in case no face was found
+
+    return face_details
